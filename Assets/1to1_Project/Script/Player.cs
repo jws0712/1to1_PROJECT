a732a18fc88c54d8e15a5f2edc5e6338eaff94ed
jws2170 <mpart2077@gmail.com>
2024-04-04 16:25:04 +0900

    [ADD] GunSpin

--- Assets/1to1_Project/Script/Player.cs
+++ Assets/1to1_Project/Script/Player.cs
@@ -5,16 +5,39 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float Speed;
+    [SerializeField] private Camera Camera;
+    [SerializeField] private GameObject HandPos;
+    private Vector3 mousePos;
     Rigidbody rb;
     Vector3 dir;
     // Start is called before the first frame update
     void Start()
     {
+        HandPos = GameObject.Find("Hand").gameObject;
+
         rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     private void Update()
+    {
+        mousePos = Camera.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector3 rot = mousePos - HandPos.transform.position;
+
+        float rotZ = Mathf.Atan2(rot.y, rot.x) * Mathf.Rad2Deg;
+
+        HandPos.transform.rotation = Quaternion.Euler(45, 0, rotZ);
+
+        PlayerDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerDirection()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveZ = Input.GetAxisRaw("Vertical");
@@ -22,7 +45,7 @@ public class Player : MonoBehaviour
         dir = new Vector3(moveX, 0, moveZ).normalized;
     }
 
-    private void FixedUpdate()
+    private void PlayerMovement()
     {
         rb.velocity = dir * Speed;
     }
